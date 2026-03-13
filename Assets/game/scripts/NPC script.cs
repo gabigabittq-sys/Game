a9da29647409486662622ec21b8b6cd21e8fdc3a
gabigabittq-sys <gabigabittq@gmail.com>
2026-03-13 10:18:12 +0200

    Initial commit

---
+++ Assets/game/scripts/NPC script.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSystem : MonoBehaviour
+{
+    bool player_detection = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(player_detection && Input.GetKeyDown(KeyCode.E))
+        {
+            print("PLEASE WORK!");
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            player_detection = true;
+        }
+    }
+    
+
+    private void OnTriggerExit(Collider other)
+    {
+        player_detection = false;
+    }
+
+}
